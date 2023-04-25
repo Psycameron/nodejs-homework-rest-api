27c3d642e57b2823d9672f5351cf61e0118a908e
Dmitriy Bulakhov <bulakhovda@gmail.com>
2023-04-25 20:05:15 +0300

    Added favorite

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -5,6 +5,7 @@ const getAll = async (req, res) => {
   const { _id: owner } = req.user;
   const page = parseInt(req.query.page) || 1;
   const limit = parseInt(req.query.limit) || 20;
+  const favorite = req.query.favorite;
 
   const options = {
     page,
@@ -16,7 +17,13 @@ const getAll = async (req, res) => {
     },
   };
 
-  const result = await Contact.paginate({ owner }, options);
+  const filter = { owner };
+
+  if (favorite) {
+    filter.favorite = favorite;
+  }
+
+  const result = await Contact.paginate(filter, options);
 
   res.json(result);
 };
